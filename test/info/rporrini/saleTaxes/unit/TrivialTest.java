36be941d028fc25ded967f73f38722d473a0ab7a
rporrini <r.porrini@gmail.com>
2015-11-20 09:50:49 +0100

    now we are able to execute our first trivial unit test

---
+++ test/info/rporrini/saleTaxes/unit/TrivialTest.java
@@ -0,0 +1,14 @@
+package info.rporrini.saleTaxes.unit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TrivialTest {
+
+	@Test
+	public void shouldAlwaysPass() {
+		assertTrue(true);
+	}
+
+}
